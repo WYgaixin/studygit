1fd86da06540cbc000f33f1be0307b99341bc51a
WYgaixin <974160815@qq.com>
2016-03-09 16:57:49 +0800

    first commit

---
+++ untitled1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 09:58:32 2015
+
+@author: ssw
+"""
+
+import untitled0
+pagelist=['http://www.baidu.com']
+crawler=untitled0.crawler('')
+crawler.crawl(pagelist)
